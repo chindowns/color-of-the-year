f4b30c5305ad4dca170f5e63e4567d5424556c0d
ChinDowns <michael@chindowns.com>
2020-09-09 21:12:09 -0700

    Updates

---
+++ precache-manifest.ad9f2ce1af2cd1d51cafe6c168d0457d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c07c9c1f4270a29baf436893f94b139",
+    "url": "/color-of-the-year/index.html"
+  },
+  {
+    "revision": "0f29a0a774a1293923e2",
+    "url": "/color-of-the-year/static/css/main.8f8ffff0.chunk.css"
+  },
+  {
+    "revision": "578ab8b9ee0c2604aa6d",
+    "url": "/color-of-the-year/static/js/2.c1d2d6dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-of-the-year/static/js/2.c1d2d6dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f29a0a774a1293923e2",
+    "url": "/color-of-the-year/static/js/main.a854cb8b.chunk.js"
+  },
+  {
+    "revision": "ccc877f4de9362984eac",
+    "url": "/color-of-the-year/static/js/runtime-main.04386476.js"
+  }
+]);
